f547e01a4f6cbad206070e314e311340f44c6ca3
Shahad Almoqbel <Shahad@promoe.local>
2023-03-03 13:52:09 +0300

    Adding ..

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,12 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css', integrity:"sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" },
+      { rel: 'stylesheet',
+       href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css', 
+       integrity:"sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" , 
+       crossorigin:"anonymous"
+      
+      },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Alegreya&display=swap' },
     {  rel: "stylesheet" , href:"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"},
     ],
@@ -21,6 +26,7 @@ export default {
       {
         src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js' ,
         integrity:"sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" ,
+        crossorigin:"anonymous"
       },
     ]
   },
